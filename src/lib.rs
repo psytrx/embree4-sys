856405a9852d924437fd6dcce57e9c227ba6aea1
Franky <psytrx@pop-os.localdomain>
2023-09-23 19:56:03 +0200

    add crate doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
+//! Rust FFI bindings for
+//! [Intel's Embree](https://www.embree.org/)
+//! 4 high-performance ray tracing library.
+//!
+//! Bindings are generated via
+//! [rust-bindgen](https://github.com/rust-lang/rust-bindgen).
+//!
+//! A valid Embree installation is required. See
+//! [Installation of Embree](https://github.com/embree/embree#installation-of-embree)
+//! from the Embree docs.
+
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
